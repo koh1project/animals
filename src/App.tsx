ca60175027bd72c477154a0b976e9383a18be53b
Koichi <koichi89592@gmail.com>
2021-01-24 19:11:14 +0900

    Change CollectionShibes directory

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import './App.css';
 
 import axios from 'axios';
 
-import CollectionShibes from './containers/CollectionShibes';
+import CollectionShibes from './containers/CollectionShibes/CollectionShibes';
 
 const URL = 'https://shibe.online/api/shibes';
 
