626c654d5a2d20000a9542e28a4da0df8b4ffafe
Koichi <koichi89592@gmail.com>
2021-01-25 18:12:25 +0900

    Make header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
-// import { HeaderContainer, AppBarContainer, ToolbarContainer, TypographyContainer } from './Header.styles';
+import { HeaderContainer, AppBarContainer, ToolbarContainer, TypographyContainer } from './Header.styles';
 
-const Header = () => <div data-test="component-header">Header</div>;
+const Header = () => (
+  <div data-test="component-header">
+    <HeaderContainer data-test="header">
+      <AppBarContainer data-test="header-appbar">
+        <ToolbarContainer data-test="header-toolbar">
+          <TypographyContainer data-test="header-typography">柴犬のいる生活</TypographyContainer>
+        </ToolbarContainer>
+      </AppBarContainer>
+    </HeaderContainer>
+  </div>
+);
 
 export default Header;
