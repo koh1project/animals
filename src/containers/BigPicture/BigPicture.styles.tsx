cbd3647d75b6c316d2219e1a4475d082b0675d57
Koichi <koichi89592@gmail.com>
2021-01-28 18:15:45 +0900

    Add big picture style with empty url

--- src/containers/BigPicture/BigPicture.styles.tsx
+++ src/containers/BigPicture/BigPicture.styles.tsx
@@ -3,9 +3,18 @@ import styled from 'styled-components';
 const BigPictureStyled = styled.img`
   height: 40vh;
   width: 40vw;
-  box-shadow: 1rem .5rem .5rem black;
+  box-shadow: 1rem 0.5rem 0.5rem black;
   margin-top: 10rem;
   margin-bottom: 5rem;
 `;
 
-export default BigPictureStyled;
+const EmptyBigPictureStyled = styled.div`
+  height: 40vh;
+  width: 40vw;
+  box-shadow: 0.5rem 0.5rem 0.5rem, 0.5rem 0.5rem 0.5rem black;
+  margin: auto;
+  margin-top: 10rem;
+  margin-bottom: 5rem;
+`;
+
+export { BigPictureStyled, EmptyBigPictureStyled };
