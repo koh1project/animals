f48603bc26e5b8b25a0271dd094ce9d1d6c175df
Koichi <koichi89592@gmail.com>
2021-02-05 18:02:21 +0900

    Delete unused files

--- src/components/GridListTile/GridListTile.styles.tsx
+++
@@ -1,4 +0,0 @@
-import styled from 'styled-components';
-import { GridListTile } from '@material-ui/core';
-
-export const CollectionItem = styled(GridListTile)``;
--- src/components/GridListTile/GridListTile.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { CollectionItem } from './GridListTile.styles';
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default (props: HTMLElement) => (
-  <React.Fragment>
-    <CollectionItem>{props.children}</CollectionItem>
-  </React.Fragment>
-);
